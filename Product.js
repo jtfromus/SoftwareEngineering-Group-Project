2d8cad3b59e03135c440d57a91a0a3484cc32149
Jonathan Tan <jonathantan1994@gmail.com>
2019-12-09 23:14:48 -0600

    added itemNumber to product class

--- Product.js
+++ Product.js
@@ -1,18 +1,21 @@
 class Product{
 
     //Constructor
-    constructor(name, price, image){
+    constructor(itemNumber, name, price, image){
+        this.itemNumber = itemNumber;
         this.name = name;
         this.price = price;
         this.image = image;
     }
 
     //getters
+    get itemNumber(){return this.itemNumber}
     get name(){return this.name;}
     get price(){return this.price;}
     get image(){return this.image;}
 
     //setters
+    set itemNumber(itemNumber){this.itemNumber = itemNumber;}
     set name(name){this.name = name;}
     set price(price){this.price = price;}
     set image(image){this.image = image;}
