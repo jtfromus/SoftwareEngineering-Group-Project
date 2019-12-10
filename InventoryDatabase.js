770ab815aeeead50596defec1d8d87306e0a8337
leeleschber <31324977+leeleschber@users.noreply.github.com>
2019-12-09 23:25:02 -0600

    Update InventoryDatabase.js
    Cleaned my code! Left comments

--- InventoryDatabase.js
+++ InventoryDatabase.js
@@ -1,31 +1,27 @@
 function createDatabase(){
 	var mySQL = require('mySQL');
-//	const readInput = require('readInput');
 
-//	readInput.readFile('Input.txt', (err, data) => {
-//		if (err)
-//			throw err;
-//		var itemInput = data.toString());
-//		var itemInputs = itemInput.val().split('\n');
-//	});
-
+	// CREATES CONFIGURATION FOR CONNECTING TO LOCAL HOST
 	var connection = mySQL.createConnection({
 		host: "localhost",
-		user: "username",
-		password: "password"
+		user: "",
+		password: ""
 	});
 
+	// CONNECTS TO LOCAL HOST 
 	connection.connect(function(err) {
 	if (err) 
 		throw err;
 	console.log("Connected!");
   
+	// CREATES DATABASE
 	connection.query("CREATE DATABASE storeDatabase", function (err, result) {
 		if (err) 
 			throw err;
 		console.log("Store Database Created");
 	});
   
+	// CREATES TABLE WITH PARAMETERS
 	// itemNumber, name, weight, expDate, price, stock, discount, description, inStock, imgSrc ./images/chicken.jpg
 	var TableCreation = "CREATE TABLE inventory (itemNumber INT PRIMARY KEY, "
 			+ "name VARCHAR(100), weight FLOAT, expDate VARCHAR(100), price FLOAT, "
@@ -37,27 +33,26 @@ function createDatabase(){
 		console.log("Created Inventory Table");
 	});
   
-//	for(var i = 0; i < itemInputs.length; i++) {
-		var TableInsert = "INSERT INTO storeDatabase (itemNumber, name, price, stock, intStock, imgSrc) VALUES ?"; //itemInputs[i];
-		var values = [
-			['987654', 'Chicken', '10.99', '25', 'TRUE', './images/chicken.jpg']
-			['135792', 'Steak', '7.99', '35', 'TRUE', './images/steak.jpg']
-			['246813', 'Fish', '8.50', '40', 'TRUE', './images/fish.jpg']
-			['345126', 'Cucumber', '0.99', '50', 'TRUE', './images/cucumber.jpg']
-			['345677', 'Tomato', '0.25', '60', 'TRUE', './images/tomato.jpg']
-			['216788', 'Lettuce', '0.15', '67', 'TRUE', './images/lettuce.jpg']
-			['332444', 'Carrot', '0.10', '56', 'TRUE', './images/carrot.jpg']
-			['867549', 'Squash', '0.08', '78', 'TRUE', './images/squash.jpg']
-			['408444', 'Apple', '0.11', '103', 'TRUE', './images/apple.jpg']
-			['408322', 'Banana', '0.10', '79', 'TRUE', './images/banana.jpg']
-			['408221', 'Orange', '0.90', '76', 'TRUE', './images/orange.jpg']
-			['900111', 'Kiwi', '0.10', '97', 'TRUE', './images/kiwi.jpg']
-		];
-		connection.query(TableInsert, [values], function (err, result) {
-			if (err)
-			throw err;
-		console.log("Entry "+ result.insertId +" inserted");
-		});
-	//}
-	});
+	// INPUTS ITEMS
+	var TableInsert = "INSERT INTO storeDatabase (itemNumber, name, price, stock, intStock, imgSrc) VALUES ?";
+	var values = [
+		['987654', 'Chicken', '10.99', '25', 'TRUE', './images/chicken.jpg']
+		['135792', 'Steak', '7.99', '35', 'TRUE', './images/steak.jpg']
+		['246813', 'Fish', '8.50', '40', 'TRUE', './images/fish.jpg']
+		['345126', 'Cucumber', '0.99', '50', 'TRUE', './images/cucumber.jpg']
+		['345677', 'Tomato', '0.25', '60', 'TRUE', './images/tomato.jpg']
+		['216788', 'Lettuce', '0.15', '67', 'TRUE', './images/lettuce.jpg']
+		['332444', 'Carrot', '0.10', '56', 'TRUE', './images/carrot.jpg']
+		['867549', 'Squash', '0.08', '78', 'TRUE', './images/squash.jpg']
+		['408444', 'Apple', '0.11', '103', 'TRUE', './images/apple.jpg']
+		['408322', 'Banana', '0.10', '79', 'TRUE', './images/banana.jpg']
+		['408221', 'Orange', '0.90', '76', 'TRUE', './images/orange.jpg']
+		['900111', 'Kiwi', '0.10', '97', 'TRUE', './images/kiwi.jpg']
+	];
+	connection.query(TableInsert, [values], function (err, result) {
+		if (err)
+		throw err;
+	console.log("Entry "+ result.insertId +" inserted");
+	});	
+});
 }
